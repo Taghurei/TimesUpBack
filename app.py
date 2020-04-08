5d0446b43250256e5da322546bcaa36b22e4cd9e
Taghurei <taghureidev@gmail.com>
2020-04-08 11:27:18 +0200

    add fullpath to db

--- app.py
+++ app.py
@@ -3,16 +3,12 @@ from routes.games import games
 from routes.players import players
 from flask_pymongo import PyMongo
 from flask_cors import CORS
-import os
-
-user = os.environ['USER']
-password = os.environ['PASSWORD']
 
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(games)
 app.register_blueprint(players)
-app.config["MONGO_URI"] = "mongodb+srv://{user}:{password}@cluster0-wam2v.mongodb.net/test?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://taghurei:taghurei@cluster0-wam2v.mongodb.net/test?retryWrites=true&w=majority"
 app.mongo = PyMongo(app)
 @app.route("/game")
 def home_page():
