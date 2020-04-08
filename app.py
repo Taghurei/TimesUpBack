c6c34aae24fc3e2a2928c0201c822cc074debafe
Taghurei <taghureidev@gmail.com>
2020-04-08 11:16:07 +0200

    add config for mlab

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from routes.games import games
 from routes.players import players
 from flask_pymongo import PyMongo
 from flask_cors import CORS
+user = os.environ['USER']
+password = os.environ['PASSWORD']
 
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(games)
 app.register_blueprint(players)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/test"
+app.config["MONGO_URI"] = "mongodb+srv://{user}:{password}@cluster0-wam2v.mongodb.net/test?retryWrites=true&w=majority"
 app.mongo = PyMongo(app)
 @app.route("/game")
 def home_page():
