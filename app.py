9096249dee60478641191078cb232ab195cae20a
Taghurei <taghureidev@gmail.com>
2020-04-09 09:39:49 +0200

    add / route

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from routes.games import games
 from routes.players import players
 from flask_pymongo import PyMongo
 from flask_cors import CORS
+from time import sleep
 
 app = Flask(__name__)
 CORS(app)
@@ -10,10 +11,6 @@ app.register_blueprint(games)
 app.register_blueprint(players)
 app.config["MONGO_URI"] = "mongodb+srv://taghurei:taghurei@cluster0-wam2v.mongodb.net/test?retryWrites=true&w=majority"
 app.mongo = PyMongo(app)
-@app.route("/game")
+@app.route("/")
 def home_page():
-    words_dict = app.mongo.db.words.find_one({"name":"GameTest"})
-    print(type(words_dict))
-    print((words_dict))
-    del words_dict['_id']
-    return json.dumps(words_dict)
+    return "Back is Up"s
