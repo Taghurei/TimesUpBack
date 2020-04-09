55767dbb53a7bde6885f79a2387e18fce2b6041b
Taghurei <taghureidev@gmail.com>
2020-04-09 10:03:20 +0200

    remove hardcoded credentials

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from routes.games import games
 from routes.players import players
 from flask_pymongo import PyMongo
 from flask_cors import CORS
+import os
 
+USER = os.environ['USER']
+PASSWORD = os.environ['PASSWORD']
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(games)
 app.register_blueprint(players)
-app.config["MONGO_URI"] = "mongodb+srv://taghurei:taghurei@cluster0-wam2v.mongodb.net/test?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://{}:{}@cluster0-wam2v.mongodb.net/test?retryWrites=true&w=majority".format(USER,PASSWORD)
 app.mongo = PyMongo(app)
 @app.route("/")
 def home_page():
